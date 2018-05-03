938cfd6d81754264cc29ddba0f7f9608ffcb63f9
B.Shorland <bshorland@bluecatnetworks.com>
2018-05-03 10:37:44 +0100

    Fix subnet delete function

--- bluecatopenstack/ipam/drivers/neutrondb_ipam/driver.py
+++ bluecatopenstack/ipam/drivers/neutrondb_ipam/driver.py
@@ -469,7 +469,7 @@ class NeutronDbPool(subnet_alloc.SubnetAllocator):
         LOG.info("BCN: Creating Network %s in BAM  ..." % (cidr))
 
         # HACK BS
-        subnet_request.name = "subnet-" + cidr 
+        subnet_request.name = "subnet-" + cidr
         bcNetID = addBCNetwork(parentBlockId, cidr, subnet_request.name, subnet_request._subnet_id, str(subnet_request._subnet_cidr.version))
 
         LOG.info("BCN: Network Added, NetworkId = %s, Name = %s UUID = %s\n" % (bcNetID, subnet_request.name, subnet_request._subnet_id))
@@ -534,7 +534,7 @@ class NeutronDbPool(subnet_alloc.SubnetAllocator):
             raise n_exc.SubnetNotFound(subnet_id=subnet_id)
 
         # BlueCat additions
-        paramsBAM = getBCNConfig(BC_configFileName, "BAM")
+        paramsBAM = getBCNConfig(BC_configFileName)
 
         if (paramsBAM['bam_updatemodify_networks'] == "True"):
 			soap_client = _bam_login(paramsBAM)
